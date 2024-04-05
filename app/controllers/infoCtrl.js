bbe72d62f742fe5cf57eded09b57cf4f69c12388
Vu Do <vudo.contact.vn@gmail.com>
2024-04-05 13:50:07 +0700

    123

--- app/controllers/infoCtrl.js
+++ app/controllers/infoCtrl.js
@@ -2,4 +2,7 @@ app.controller('infoCtrl' , function($scope) {
     $scope.updateInfo = function() {
         alert('Cập nhật thông tin thành công!')
     }
+    $scope.updatePass = function() {
+        alert('Đổi mật khẩu thành công!')
+    }
 })
